9d3e67414ceb64d1e2d39b3bb6d8c9947440b77e
Danny Aziz <daanyaal997@gmail.com>
2020-03-12 17:13:22 +0000

    💩 Create init functions

--- hiveone_py/__init__.py
+++ hiveone_py/__init__.py
@@ -1,2 +1,24 @@
-def test():
-    print("Hello World")
+class Hive:
+    def __init__(self, api_key, default_format = 'screen_name', host = 'https://hive.one/'):
+        self.api_key = api_key
+        self.default_format = default_format
+        self.host = host
+    
+
+    def available_influencers(self):
+        pass
+
+    def top_influencers(self):
+        pass
+    
+    def influencer_details(self):
+        pass
+    
+    def influencer_history(self):
+        pass
+    
+    def influencer_podcasts(self):
+        pass
+    
+    def influencer_batch(self):
+        pass
